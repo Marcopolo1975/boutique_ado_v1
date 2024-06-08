dae92909ad212ad4334f62f8659797bba0809757
Marcopolo1975 <140216383+Marcopolo1975@users.noreply.github.com>
2024-06-08 02:39:35 +0200

    add heroku.com to allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost',  '127.0.0.1:8000', 'boutique-ado1.herokuapp.com',]
+ALLOWED_HOSTS = ['localhost', ".herokuapp.com",  '127.0.0.1:8000', 'boutique-ado1.herokuapp.com',]
 
 
 # Application definition
